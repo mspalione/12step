f2b1dc2b9cce47f98e6844f897ff59b7e52815cc
Melissa Spalione <mspalione@strategicmarketing.com>
2021-03-04 15:25:53 -0500

    initial proj structure

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api
+ * @group foo - Operations about user
+ * @param {string} email.query.required - username or email - eg: user@domain
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/', (req, res) => {
+    res.send('wassup')
+})
+
+module.exports = router
