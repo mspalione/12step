45102a6648ffc4d195fb199fa0ea97bf82e632b1
Melissa Spalione <mspalione@strategicmarketing.com>
2021-03-04 20:28:03 -0500

    swagger clean-up

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = require('./routes')
 
 const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+
+app.use((req, res, next) => {
+    // allow requests from swagger
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3001')
+    next()
+})
 
 app.use('/', router)
 
